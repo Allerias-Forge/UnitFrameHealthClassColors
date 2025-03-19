ca88da28c090b6b43a431301b25f77e6536346c3
ThiconZ <ThiconZ@users.noreply.github.com>
2025-03-19 17:15:41 -0400

    Further prevent health bar colors from resetting

--- UnitFrameHealthClassColors/UnitFrameHealthClassColors.lua
+++ UnitFrameHealthClassColors/UnitFrameHealthClassColors.lua
@@ -2,6 +2,12 @@ local _G = _G or getfenv(0)
 
 UnitFrameHealthClassColors = CreateFrame("Frame", nil, UIParent);
 
+local function UpdatePlayerHealthBar()
+	local _, class = UnitClass("player");
+	local classColor = RAID_CLASS_COLORS[class] or { r = 0, g = 1, b = 0, a = 1 };
+	PlayerFrameHealthBar:SetStatusBarColor(classColor.r, classColor.g, classColor.b, 1);
+end
+
 local function UpdatePartyHealthBars()
 	if GetNumPartyMembers() > 0 then
 		for idx = 1, MAX_PARTY_MEMBERS do
@@ -32,14 +38,9 @@ local function UpdateTargetofTargetHealthBar()
 		end
 	end
 
-	-- Reupdate the target's color because sometimes it changes back to normal for some reason
+	-- Reupdate the player and target's color because sometimes it changes back to normal for some reason
 	UpdateTargetHealthBar();
-end
-
-local function UpdatePlayerHealthBar()
-	local _, class = UnitClass("player");
-	local classColor = RAID_CLASS_COLORS[class] or { r = 0, g = 1, b = 0, a = 1 };
-	PlayerFrameHealthBar:SetStatusBarColor(classColor.r, classColor.g, classColor.b, 1);
+	UpdatePlayerHealthBar();
 end
 
 UnitFrameHealthClassColors:SetScript("OnEvent", function()
@@ -50,6 +51,15 @@ UnitFrameHealthClassColors:SetScript("OnEvent", function()
 		UpdateTargetHealthBar();
 	elseif event == "PARTY_MEMBERS_CHANGED" then
 		UpdatePartyHealthBars();
+	elseif event == "UNIT_HEALTH" then
+		-- Sometimes health bars get their color reset, it's unclear why but this should help fix it
+		if arg1 == "player" then
+			UpdatePlayerHealthBar();
+		elseif arg1 == "target" or arg1 == "targettarget" then
+			UpdateTargetofTargetHealthBar();
+		elseif arg1 == "party1" or arg1 == "party2" or arg1 == "party3" or arg1 == "party4" then
+			UpdatePartyHealthBars();
+		end
 	end
 end);
 
@@ -62,3 +72,4 @@ end
 UnitFrameHealthClassColors:RegisterEvent("PLAYER_ENTERING_WORLD");
 UnitFrameHealthClassColors:RegisterEvent("PLAYER_TARGET_CHANGED");
 UnitFrameHealthClassColors:RegisterEvent("PARTY_MEMBERS_CHANGED");
+UnitFrameHealthClassColors:RegisterEvent("UNIT_HEALTH");
